53ae50ef2aa6602567e83467f464164b451f1e71
Henrique Divino <henriquedivino55555@gmail.com>
2025-05-10 18:17:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,4 @@ botao.addEventListener('click',e =>{
     botao.style.color = 'darkcyan';
     botao.innerHTML = 'Continue trabalhando árduamente e sendo uma boa pessoa, pois por cada uma delas, terá sua plena recompensa';
     botao.style.border = 'none';
-    botao.border
 });
