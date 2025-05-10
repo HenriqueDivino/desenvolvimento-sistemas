2c97d0782be2c37c6061b89f0774afd2cb0951bc
Henrique Divino <henriquedivino55555@gmail.com>
2025-05-10 17:06:05 -0300

    Conteúdo completo de Site Pessoal
    Contém link pertinente, códigos HTML, CSS , JavaScript e imagens utilizadas para formação de site simples.

---
+++ script.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector('.botao');
+
+botao.addEventListener('click',e =>{
+    botao.style.background = 'none';
+    botao.style.color = 'darkcyan';
+    botao.innerHTML = 'Continue trabalhando árduamente e sendo uma boa pessoa, pois por cada uma delas, terá sua plena recompensa';
+    botao.style.border = 'none';
+    botao.border
+});
